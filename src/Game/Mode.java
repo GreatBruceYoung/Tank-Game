b0634650d8ec4f73fef2880f8db0e278202152a1
Bruce Young <58630245+GreatBruceYoung@users.noreply.github.com>
2021-01-17 22:01:32 +0800

    Add files via upload

---
+++ src/Game/Mode.java
@@ -0,0 +1,6 @@
+package Game;
+
+public enum Mode {
+	Single,Double;
+
+}
---
+++ src/Game/Wall.java
@@ -0,0 +1,15 @@
+package Game;
+
+/**
+ * 游戏的各种方块的抽象
+ */
+class Wall extends MyImage{
+
+    //识别为那种方块的id
+	int id;
+
+	Wall(Coord coord,int id){
+		super(coord);
+		this.id = id;
+	}
+}
